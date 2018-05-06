dc41a59d440a6a24ae661d33f9b45f1d09866af0
HanieTran <tnghanit@gmail.com>
2018-05-06 22:54:59 +0700

    tin tuc, chua xòng

---
+++ database/migrations/2018_05_06_134258_create_news_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->string('title',255);
+            $table->number('ordinal_display');
+            $table->integer('created_by')->unsigned();;
+            $table->string('proof_type')->unique()->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('news');
+    }
+}
