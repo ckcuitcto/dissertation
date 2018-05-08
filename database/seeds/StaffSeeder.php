eaa036f53fd0d93103e36bcc1e4656dcf74c4b3e
thai.duc <huynhjduc248@gmail.com>
2018-05-08 08:04:04 +0700

    evaluation form

--- database/seeds/StaffSeeder.php
+++ database/seeds/StaffSeeder.php
@@ -13,40 +13,65 @@ class StaffSeeder extends Seeder
     {
         DB::table('users')->insert([
             [
-                'id' => 'GV123123',
-                'name' => 'Cố vấn học tập',
-                'email' => 'GV123123@gmail.com',
-                'password' => bcrypt('GV123123'),
-                'role_id' => 3
+                'id' => 'GVIT000001',
+                'name' => 'CVHT Khoa IT 1',
+                'email' => 'GVIT000001@gmail.com',
+                'password' => bcrypt('GVIT000001'),
+                'role_id' => 3,
+                'faculty_id' => 1
             ],
             [
-                'id' => 'GV124124',
-                'name' => 'Ban chủ nhiệm khoa',
-                'email' => 'GV122223@gmail1.com',
-                'password' => bcrypt('GV124124'),
-                'role_id' => 4
+                'id' => 'GVIT000002',
+                'name' => 'CVHT Khoa IT 2',
+                'email' => 'GVIT000002@gmail1.com',
+                'password' => bcrypt('GVIT000002'),
+                'role_id' => 3,
+                'faculty_id' => 1
+            ],[
+                'id' => 'GVIT000003',
+                'name' => 'Ban chủ nhiệm Khoa IT 3',
+                'email' => 'GVIT000003@gmail1.com',
+                'password' => bcrypt('GVIT000003'),
+                'role_id' => 3,
+                'faculty_id' => 1
             ],
             [
-                'id' => 'CT123123',
-                'name' => 'Giao vien han',
-                'email' => 'GV1241325@gmail1.com',
-                'password' => bcrypt('GV124125'),
-                'role_id' => 5
+                'id' => 'CTSV000004',
+                'name' => 'Phòng CTSV 4',
+                'email' => 'CTSV000004@gmail1.com',
+                'password' => bcrypt('CTSV000004'),
+                'role_id' => 5,
+                'faculty_id' => null
+            ],[
+                'id' => 'CTSV000005',
+                'name' => 'Phòng CTSV 5',
+                'email' => 'CTSV000005@gmail1.com',
+                'password' => bcrypt('CTSV000005'),
+                'role_id' => 5,
+                'faculty_id' => null
             ],
             [
-                'id' => 'GV124125',
-                'name' => 'Cố vấn học tập',
-                'email' => 'GV1241225@gmail1.com',
-                'password' => bcrypt('GV124125'),
-                'role_id' => 3
+                'id' => 'GVQT000006',
+                'name' => 'Ban chủ nhiệm Khoa QTKD 6',
+                'email' => 'GVQT000006@gmail1.com',
+                'password' => bcrypt('GVQT000006'),
+                'role_id' => 4,
+                'faculty_id' => 2
             ],
-
             [
-                'id' => 'AD123122',
-                'name' => 'V . I . P',
-                'email' => 'huynhjduc248@gmail1.com',
-                'password' => bcrypt('admin123'),
-                'role_id' => 6
+                'id' => 'GVQT000007',
+                'name' => 'CVHT Khoa QTKD 7',
+                'email' => 'GVQT000007@gmail1.com',
+                'password' => bcrypt('GVQT000007'),
+                'role_id' => 3,
+                'faculty_id' => 2
+            ],[
+                'id' => 'GVQT000008',
+                'name' => 'CVHT Khoa QTKD 8',
+                'email' => 'GVQT000008@gmail1.com',
+                'password' => bcrypt('GVQT000008'),
+                'role_id' => 3,
+                'faculty_id' => 2
             ]
         ]);
     }
