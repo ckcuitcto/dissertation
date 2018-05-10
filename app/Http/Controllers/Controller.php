86e48e61bf71b0d9818f09820cd125bb5bf6844f
thai.duc <huynhjduc248@gmail.com>
2018-05-11 01:00:23 +0700

    fix lỗi hiển thị ở danh sách

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -17,12 +17,16 @@ class Controller extends BaseController
         if ($user->Role->id >= 5) // admin va phong ctsv thì lấy tất cả user
         {
             $students = Student::all();
+//            $students = Student::whereHas('users', function ($query) {
+//                $query->where('role_id', '<=', 2);
+//            })->get();
+            return $students;
         } elseif ($user->Role->id >= 2) //ban can su lop, co van hoc tpa, chu nhiem khoa thì lấy user thuộc khoa giống
         {
             $users = array_flatten(User::where('faculty_id', $user->faculty_id)->where('role_id', '<=', 2)->select('id')->get()->toArray());
             $students = Student::whereIn('user_id', $users)->get();
+            return $students;
         }
 
-        return $students;
     }
 }
