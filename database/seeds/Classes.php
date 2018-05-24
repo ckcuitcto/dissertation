31d9ec755a9a0dd3d78ef12ca9e4c684c13c700a
HanieTran <tnghanit@gmail.com>
2018-05-24 21:51:12 +0700

    seeder class thêm lớp mới vào

--- database/seeds/Classes.php
+++ database/seeds/Classes.php
@@ -15,8 +15,22 @@ class Classes extends Seeder
             ['name' => 'D14-TH01','faculty_id' => '1', 'staff_id' => '1'],
             ['name' => 'D14-TH02','faculty_id' => '1', 'staff_id' => '1'],
             ['name' => 'D14-TH03','faculty_id' => '1', 'staff_id' => '2'],
+            ['name' => 'D14-TH04','faculty_id' => '1', 'staff_id' => '2'],
+            ['name' => 'D14-TH05','faculty_id' => '1', 'staff_id' => '3'],
+            ['name' => 'D14-TH06','faculty_id' => '1', 'staff_id' => '3'],
+            ['name' => 'D15-TH01','faculty_id' => '1', 'staff_id' => '1'],
+            ['name' => 'D15-TH02','faculty_id' => '1', 'staff_id' => '1'],
+            ['name' => 'D15-TH03','faculty_id' => '1', 'staff_id' => '1'],
+            ['name' => 'D15-TH04','faculty_id' => '1', 'staff_id' => '2'],
+            ['name' => 'D15-TH05','faculty_id' => '1', 'staff_id' => '2'],
+            ['name' => 'D15-TH06','faculty_id' => '1', 'staff_id' => '2'],
+            ['name' => 'D15-TH07','faculty_id' => '1', 'staff_id' => '3'],
+            ['name' => 'D15-TH08','faculty_id' => '1', 'staff_id' => '3'],
+            ['name' => 'D15-TH09','faculty_id' => '1', 'staff_id' => '3'],
+            ['name' => 'D15-TH010','faculty_id' => '1', 'staff_id' => '3'],
             ['name' => 'D14-QT01','faculty_id' => '2', 'staff_id' => '7'],
             ['name' => 'D14-QT02','faculty_id' => '2', 'staff_id' => '8'],
+            
         ]);
     }
 }
