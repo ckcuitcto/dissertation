0e07b8674e60ec31dccd9aaa0259081c24d51731
HanieTran <tnghanit@gmail.com>
2018-06-15 20:01:42 +0700

    test

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -332,6 +332,24 @@
     {{--<script type="text/javascript">$('#sampleTable').DataTable();</script>--}}
 
     <script>
+        // $(document).ready(function() {
+        // $(".submit").click(function() {
+        //     $(".submit").addClass("loading");
+        //     setTimeout(function() {
+        //     $(".submit").addClass("hide-loading");
+        //     // For failed icon just replace ".done" with ".failed"
+        //     $(".done").addClass("finish");
+        //     }, 3000);
+        //     setTimeout(function() {
+        //     $(".submit").removeClass("loading");
+        //     $(".submit").removeClass("hide-loading");
+        //     $(".done").removeClass("finish");
+        //     $(".failed").removeClass("finish");
+        //     }, 5000);
+        // })
+        // });
+
+
         $(document).ready(function () {
 
             var oTable = $('#table-users').DataTable({
