73ef104b214db9e9110c44e1304a1f5c5c1179e0
thai.duc <huynhjduc248@gmail.com>
2018-07-25 21:29:01 +0700

    semester dang bij loi

--- app/Http/Controllers/Semester/SemesterController.php
+++ app/Http/Controllers/Semester/SemesterController.php
@@ -430,9 +430,9 @@ class SemesterController extends Controller
                 $btnEdit = "<a style='color: white' title='Sửa học kì' data-semester-id='$semester->id' data-semester-edit-link='$linkEdit'
                 data-semester-update-link='$linkUpdate' class='btn btn-primary update-semester'> <i class='fa fa-edit' aria-hidden='true'></i> </a>";
 
-                $btnDelete = "<a style='color: white' title='Xóa học kì' data-semester-id='$semester->id'  data-semester-delete-link='$linkDestroy'
-                class='btn btn-danger destroy-semester'> <i class='fa fa-trash' aria-hidden='true'></i></a>";
-                return "<span class='bs-component'>$btnEdit $btnDelete</span> ";
+//                $btnDelete = "<a style='color: white' title='Xóa học kì' data-semester-id='$semester->id'  data-semester-delete-link='$linkDestroy'
+//                class='btn btn-danger destroy-semester'> <i class='fa fa-trash' aria-hidden='true'></i></a>";
+                return "<span class='bs-component'>$btnEdit</span> ";
 
             })
             ->setRowClass(function ($semester) use ($currentSemester) {
@@ -530,5 +530,129 @@ class SemesterController extends Controller
         }
 
         return $arrMessage;
+    }
+
+    public function ajaxGetSemestersForBackupImportant()
+    {
+        $currentSemester = $this->getCurrentSemester();
+        $semesters = DB::table('semesters')
+            ->select(
+                '*',
+                DB::raw("CONCAT(semesters.year_from,'-',semesters.year_to) as semesterYear")
+            );
+
+        return DataTables::of($semesters)
+        ->addColumn('action', function ($semester) {
+            $linkDestroy = route('backup-important-handle', $semester->id);
+            $btnDelete = "<a style='color: white' title='Xóa học kì' data-semester-id='$semester->id'  data-semester-delete-link='$linkDestroy'
+            class='btn btn-danger destroy-semester'> <i class='fa fa-trash' aria-hidden='true'></i></a>";
+            return "<span class='bs-component'>$btnDelete</span> ";
+
+        })
+        ->setRowClass(function ($semester) use ($currentSemester) {
+            return $semester->id == $currentSemester->id ? 'alert-success' : '';
+        })
+        ->make(true);
+    }
+
+
+    public function backupImportant(){
+        return view('backup.backup-important');
+    }
+
+    public function backupImportantHandle($id){
+        $semester = Semester::find($id);
+        if (!empty($semester)) {
+            $user      = env("DB_USERNAME", "root");
+            $pass      = env("DB_PASSWORD", "");
+            $host      = env("DB_HOST", "localhost");
+            $name             = env("DB_DATABASE", "db24");
+            $backup_name      = "backup".date("dmY")."sql";
+            $tables           = "Your tables";
+
+
+//        $mysqli = new mysqli($host,$user,$pass,$name);
+//        $mysqli->select_db($name);
+//        $mysqli->query("SET NAMES 'utf8'");
+
+            $queryTables    = $results = DB::select('SHOW TABLES');
+            foreach ($queryTables as $row)
+            {
+                $target_tables[] = array_flatten((array)$row)[0];
+            }
+//        if($tables !== false)
+//        {
+//            $target_tables = array_intersect( $target_tables, $tables);
+//        }
+            foreach($target_tables as $table)
+            {
+//                $result         =   $mysqli->query('SELECT * FROM '.$table);
+                $result         =   DB::table($table)->select("*");
+                $fields_amount  =   $result->field_count;
+                $rows_num= $mysqli->affected_rows;
+                $res            =   $mysqli->query('SHOW CREATE TABLE '.$table);
+                $TableMLine     =   $res->fetch_row();
+                $content        = (!isset($content) ?  '' : $content) . "\n\n".$TableMLine[1].";\n\n";
+
+                for ($i = 0, $st_counter = 0; $i < $fields_amount;   $i++, $st_counter=0)
+                {
+                    while($row = $result->fetch_row())
+                    { //when started (and every after 100 command cycle):
+                        if ($st_counter%100 == 0 || $st_counter == 0 )
+                        {
+                            $content .= "\nINSERT INTO ".$table." VALUES";
+                        }
+                        $content .= "\n(";
+                        for($j=0; $j<$fields_amount; $j++)
+                        {
+                            $row[$j] = str_replace("\n","\\n", addslashes($row[$j]) );
+                            if (isset($row[$j]))
+                            {
+                                $content .= '"'.$row[$j].'"' ;
+                            }
+                            else
+                            {
+                                $content .= '""';
+                            }
+                            if ($j<($fields_amount-1))
+                            {
+                                $content.= ',';
+                            }
+                        }
+                        $content .=")";
+                        //every after 100 command cycle [or at last line] ....p.s. but should be inserted 1 cycle eariler
+                        if ( (($st_counter+1)%100==0 && $st_counter!=0) || $st_counter+1==$rows_num)
+                        {
+                            $content .= ";";
+                        }
+                        else
+                        {
+                            $content .= ",";
+                        }
+                        $st_counter=$st_counter+1;
+                    }
+                } $content .="\n\n\n";
+            }
+            //$backup_name = $backup_name ? $backup_name : $name."___(".date('H-i-s')."_".date('d-m-Y').")__rand".rand(1,11111111).".sql";
+            $backup_name = $backup_name ? $backup_name : $name.".sql";
+            header('Content-Type: application/octet-stream');
+            header("Content-Transfer-Encoding: Binary");
+            header("Content-disposition: attachment; filename=\"".$backup_name."\"");
+            echo $content; exit;
+
+            $semester->delete();
+            return response()->json([
+                'semester' => $semester,
+                'status' => true
+            ], 200);
+            return response()->json([
+                'status' => false,
+                'message' => 'Không được xóa học kì này'
+            ], 200);
+        }
+        return response()->json([
+            'status' => false,
+            'message' => 'Không tìm thấy học kì'
+        ], 200);
     }
 }
